12cb1d941b64c51c23f63eb7eadffc4ca298e8e1
rommuel <rommuelrivera3@gmail.com>
2022-07-11 21:34:25 +0800

    Forgot to commit about the json reading process, but tried a code to read the json file. Exceptions are still encountered. Trying to find the mistake on the json or the lack of needed libraries used.

---
+++ PDF Resume Creator/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace PDF_Resume_Creator
+{
+    [Serializable]
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Birthday { get; set; }
+        
+    
+    }
+}
--- PDF Resume Creator/Form1.cs
+++ PDF Resume Creator/Form1.cs
@@ -3,12 +3,51 @@ using PdfSharp.Pdf;
 using PdfSharp.Drawing;
 using System.Diagnostics;
 using System.Text;
+using System.Text.Json;
+
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace PDF_Resume_Creator
 
 {
+    
+    public class Resumeinfo 
+    { 
+    
+        //Basic Information
+        public string Name { get; set; }
+        public string Birthday { get; set; }
+        public string Contact_Number { get; set; }
+        public string Email_Address { get; set; }
+        public string Address { get; set; }
+
+        //Educational Background
+        public string College { get; set; }
+        public string SeniorHighSchool { get; set; }
+        public string JuniorHighSchool { get; set; }
+        
+
+        //Awards and Certificates
+        public string Award1 { get; set; }
+        public string Award2 { get; set; }
+        public string Certificate1 { get; set; }
+        public string Certificate2 { get; set; }
+
+        //Skills
+        public string Skill1 { get; set; }
+        public string Skill2 { get; set; }
+        public string Skill3 { get; set; }
+        public string Skill4 { get; set; }
+        public string Skill5 { get; set; }
 
 
+    }
+
+  
+    
 
     public partial class Form1 : Form
     {
@@ -16,6 +55,11 @@ namespace PDF_Resume_Creator
         {
             InitializeComponent();
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            string filename = "resume.json";
+            string jsonstring = File.ReadAllText(filename);
+            Resumeinfo resumeinfo = JsonSerializer.Deserialize<Resumeinfo>(jsonstring)!;
+            
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
